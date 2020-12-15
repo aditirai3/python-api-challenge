cb6e66da53f6a395a9018c4083a9f39930172d9b
Aditi Rai <aditirai@Aditis-Air.lan>
2020-12-15 18:25:50 +0530

    added api keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8c90e711a859130789146c45f2734334"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD9Xy8f-oCCQHH_dlrAMiyEXpg_xe6aLMs"
+
+
